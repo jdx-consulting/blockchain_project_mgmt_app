562718388acbd32a4bc5fa02b164c53c31fdd448
Matt Steward <matthew.steward.it@gmail.com>
2018-04-12 14:42:54 +0100

    Test Notification

--- App/web/src/js/index.js
+++ App/web/src/js/index.js
@@ -1,10 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Welcome, Hello } from "./app";
+import { OpenFinVersion } from "./layout/footer";
 import style from "../main.css";
 
 console.log('In Index.js');
 
+const PopUp = () => {
+    
+
+    var notification = new fin.desktop.Notification({
+        //The HTML page containing the content of the Notification/
+        url: "http://localhost:8080/web/src/notifications/appStarted.html",
+        //'message' is where you may pass initial parameters
+        message: { lab: "Check out da' BBC...", link: "http://www.bbc.co.uk" },
+        onClick: function () {
+            console.log("clicked");
+        },
+        onClose: function () {
+        // Invoked when the 'close()' method is called.
+            console.log("closed");
+        },
+        onDismiss: function () {
+        //This is invoked when the notification is swiped right to dismiss. 
+        //It is esentially the same as clove via a different method.
+            console.log("dismissed");
+        },
+        onError: function (reason) {
+            console.log("error: " + reason);
+        },
+        onMessage: function (message) {
+        //Invoked when the 
+            console.log("message: ", message);
+        },
+        onShow: function () {
+            console.log("shown");
+        }
+    });
+};
 
 var isOpenFin = ('fin' in window && 'desktop' in fin);
 
@@ -17,7 +50,19 @@ if(isOpenFin) {
         console.log('OpenFin Version: ' + version);
         //const ofVersion = document.querySelector('#of-version');
         //ofVersion.innerText = version;
+
+        ReactDOM.render(
+            <OpenFinVersion openFinVersion={version} />,
+            document.getElementById('theFooter')
+            );
     });
+    PopUp();
+}
+else {
+    ReactDOM.render(
+            <h1>Browser.</h1>,
+            document.getElementById('theFooter')
+            );
 }
 
 ReactDOM.render(
@@ -29,6 +74,8 @@ ReactDOM.render(
   <Welcome name="Matthew" />,
   document.getElementById('app')
 );
+
+
 
 
 console.log('Out Index.js');
---
+++ App/web/src/js/layout/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const OpenFinVersion = (props) => { 
+    
+    return <h1>OpenFin: {props.openFinVersion}</h1>
+};
+
+
+
